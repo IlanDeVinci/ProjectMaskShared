5bdf795421d0adc6812ed90ebf88b9b7b59a58df
IlanDeVinci <ilan.maouchi@gmail.com>
2024-05-04 16:39:20 +0200

    Commit

---
+++ Assets/SSL/Runtime/Scripts/Misc/ReloadSceneOnR.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadSceneOnR : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
---
+++ Assets/SSL/Runtime/Scripts/SpeedExamples/ApplySpeedVectorExample.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ApplySpeedVectorExample : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _velocity
+        = new Vector3(5f, 2f, 0f);
+
+    private void Update()
+    {
+        transform.position += _velocity * Time.deltaTime;
+    }
+}
+
